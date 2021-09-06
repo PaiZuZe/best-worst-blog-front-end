7bb76eed1372abb4ae5cdf430351a8160165af6d
PaiZuZe <victorcga96@gmail.com>
2021-09-06 14:09:30 -0300

    Adds Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorDetailComponent } from './author-detail/author-detail.component';
@@ -14,6 +19,7 @@ import { HomeComponent } from './home/home.component';
 import { BlogPostsComponent } from './blog-posts/blog-posts.component';
 import { BlogPostCreatorComponent } from './blog-post-creator/blog-post-creator.component';
 import { BlogPostDetailComponent } from './blog-post-detail/blog-post-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -32,7 +38,12 @@ import { BlogPostDetailComponent } from './blog-post-detail/blog-post-detail.com
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
